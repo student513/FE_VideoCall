57bb463f897264de8a906bd83ad6ec13ee659f8b
jojo <compa513@naver.com>
2020-11-04 20:59:18 +0900

    get video Id, title & push to array

--- src/Room.js
+++ src/Room.js
@@ -1,35 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Video from 'twilio-video';
 import Participant from './Participant';
 import YouTube from 'react-youtube';
+
+const getVideoId = require('get-video-id');
+const getVideoTitle = require('get-youtube-title');
 
 const opts = {
   height: '390',
   width: '640',
   playerVars: {
     // https://developers.google.com/youtube/player_parameters
-    autoplay: 1,
-  }
-}
+    autoplay: 0,
+  },
+};
 
 const Room = ({ roomName, token, handleLogout }) => {
   const [room, setRoom] = useState(null);
   const [participants, setParticipants] = useState([]);
+  const [videoInfoList, setVideoInfoList] = useState([]);
+  const [url, setUrl] = useState('');
+
+  const nextId = useRef(0);
+
+  const onChangeUrl = (e) => {
+    setUrl(e.target.value);
+  };
+  const onPushToList = () => {
+    const videoId = getVideoId(url).id;
+    getVideoTitle(videoId, function (err, title) {
+      console.log(err);
+      console.log(title);
+      setVideoInfoList((prevInfo) => [
+        ...prevInfo,
+        { url, videoId, title, id: nextId.current++ },
+      ]);
+    });
+
+    console.log(videoInfoList);
+    setUrl('');
+  };
 
   useEffect(() => {
-    const participantConnected = participant => {
-      setParticipants(prevParticipants => [...prevParticipants, participant]);
+    const participantConnected = (participant) => {
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
     };
 
-    const participantDisconnected = participant => {
-      setParticipants(prevParticipants =>
-        prevParticipants.filter(p => p !== participant)
+    const participantDisconnected = (participant) => {
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
       );
     };
 
     Video.connect(token, {
-      name: roomName
-    }).then(room => {
+      name: roomName,
+    }).then((room) => {
       setRoom(room);
       room.on('participantConnected', participantConnected);
       room.on('participantDisconnected', participantDisconnected);
@@ -37,9 +62,11 @@ const Room = ({ roomName, token, handleLogout }) => {
     });
 
     return () => {
-      setRoom(currentRoom => {
+      setRoom((currentRoom) => {
         if (currentRoom && currentRoom.localParticipant.state === 'connected') {
-          currentRoom.localParticipant.tracks.forEach(function(trackPublication) {
+          currentRoom.localParticipant.tracks.forEach(function (
+            trackPublication
+          ) {
             trackPublication.track.stop();
           });
           currentRoom.disconnect();
@@ -51,13 +78,12 @@ const Room = ({ roomName, token, handleLogout }) => {
     };
   }, [roomName, token]);
 
-  const remoteParticipants = participants.map(participant => (
+  const remoteParticipants = participants.map((participant) => (
     <Participant key={participant.sid} participant={participant} />
   ));
 
   return (
     <div className="room">
-      <h2>Room: {roomName}</h2>
       <button onClick={handleLogout}>Log out</button>
       <YouTube videoId="2g811Eo7K8U" opts={opts} />
       <div className="local-participant">
@@ -69,6 +95,17 @@ const Room = ({ roomName, token, handleLogout }) => {
         ) : (
           ''
         )}
+      </div>
+      <div>
+        {videoInfoList.map((videoInfo) => (
+          <p key={videoInfo.id}>{videoInfo.title}</p>
+        ))}
+        <input
+          value={url}
+          onChange={onChangeUrl}
+          placeholder="동영상 링크를 입력하세요."
+        />
+        <button onClick={onPushToList}>제출</button>
       </div>
       <h3>Remote Participants</h3>
       <div className="remote-participants">{remoteParticipants}</div>
--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,51 +1,51 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import Lobby from './Lobby';
-import Room from './Room';
+import React, { useState, useCallback, useEffect } from 'react'
+import Lobby from './Lobby'
+import Room from './Room'
 
 const VideoChat = () => {
-  const [username, setUsername] = useState('');
-  const [roomName, setRoomName] = useState('');
-  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState('')
+  const [roomName, setRoomName] = useState('')
+  const [token, setToken] = useState(null)
 
-  const handleUsernameChange = useCallback(event => {
-    setUsername(event.target.value);
-  }, []);
+  const handleUsernameChange = useCallback((event) => {
+    setUsername(event.target.value)
+  }, [])
 
-  const handleRoomNameChange = useCallback(event => {
-    setRoomName(event.target.value);
-  }, []);
+  const handleRoomNameChange = useCallback((event) => {
+    setRoomName(event.target.value)
+  }, [])
 
   const handleSubmit = useCallback(
-    async event => {
-      event.preventDefault();
+    async (event) => {
+      event.preventDefault()
       const data = await fetch('/video/token', {
         method: 'POST',
         body: JSON.stringify({
           identity: username,
-          room: roomName
+          room: roomName,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json());
-      setToken(data.token);
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json())
+      setToken(data.token)
     },
     [roomName, username]
-  );
+  )
 
-  const handleLogout = useCallback(event => {
-    setToken(null);
-  }, []);
+  const handleLogout = useCallback((event) => {
+    setToken(null)
+  }, [])
 
   useEffect(() => {
     console.log(token)
   })
 
-  let render;
+  let render
   if (token) {
     render = (
       <Room roomName={roomName} token={token} handleLogout={handleLogout} />
-    );
+    )
   } else {
     render = (
       <Lobby
@@ -55,9 +55,9 @@ const VideoChat = () => {
         handleRoomNameChange={handleRoomNameChange}
         handleSubmit={handleSubmit}
       />
-    );
+    )
   }
-  return render;
-};
+  return render
+}
 
-export default VideoChat;
+export default VideoChat
